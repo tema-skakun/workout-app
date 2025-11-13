e20d4a9d776e44e9bf70b2dd54ca01fc5d7e21f2
Artem Skakun <tema_skakun@mail.ru>
2025-11-13 16:27:04 +0300

    fix: fixes header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
+import {useTranslation} from "react-i18next";
+
 export default function Footer() {
+	const {t} = useTranslation();
+
 	return (
-		<footer>
-			<p>© {new Date().getFullYear()} Workout App</p>
+		<footer style={{margin: 0, color: 'black'}}>
+			<p>© {new Date().getFullYear()} {t('appTitle')}</p>
 		</footer>
 	);
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,25 @@
-import Navbar from './Navbar';
 import {useAuth} from '@/auth/AuthContext';
-import {useTranslation} from 'react-i18next';
-
+import { useTranslation } from 'react-i18next';
+import Navbar from './Navbar';
 
 export default function Header() {
 	const {t} = useTranslation();
 	const {user, logout} = useAuth();
 	return (
 		<header>
-			<h3 style={{margin: 0}}>{t('appTitle')}</h3>
-			<div style={{marginLeft: 'auto'}}>
-				{user ? (
-					<>
-						<Navbar/>
-						<button className="btn" onClick={logout}>{t('nav.logout')}</button>
-					</>
-				) : null}
-			</div>
+			<h3 style={{margin: 0, color: 'black'}}>{t('appTitle')}</h3>
+			{user ? (
+				<>
+					<Navbar/>
+					<button
+						className="btn"
+						onClick={logout}
+						style={{marginLeft: 'auto'}}
+					>
+						{t('nav.logout')}
+					</button>
+				</>
+			) : null}
 		</header>
 	);
 }
